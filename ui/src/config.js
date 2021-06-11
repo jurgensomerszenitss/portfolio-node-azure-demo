ad55910f8ad078e13eb8e7bbfbaeabe1bfe46447
Jurgen Somers <78384001+jurgensomerszenitss@users.noreply.github.com>
2021-06-11 19:09:12 +0300

    Update config.js

--- ui/src/config.js
+++ ui/src/config.js
@@ -13,15 +13,3 @@ export const AuthSettings = {
 export const ApiSettings = { 
     VUE_APP_API : '#{VUE_APP_API}#'
 }
-
-
-/* local : uncomment this section to run local (development)*/
-// export const AuthSettings = {
-//     VUE_APP_AUTH_CLIENT_ID : '2eb44e3d-6e68-4a77-9e7d-ae045182d3ef',
-//     VUE_APP_AUTH_AUTHORITY : 'https://login.microsoftonline.com/12a3af23-a769-4654-847f-958f3d479f4a', 
-//     VUE_APP_AUTH_REDIRECT_URI : 'https://localhost:4200'
-// }
- 
-// export const ApiSettings = {
-//     VUE_APP_API : 'http://localhost:3200/'
-// } 
